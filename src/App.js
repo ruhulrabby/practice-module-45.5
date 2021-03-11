5b7067ba77160fdf1eb0c5f20914cd1d35f229f6
ruhul rabby <ruhulalam1994@gmail.com>
2021-03-12 05:16:57 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   const [likeColor, setLikeColor] = useState('');
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+  useEffect( () =>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+
+      //single user
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then(res=> res.json())
+      .then(data=> setSingleUser(data));
+
+      //random user
+      fetch('https://randomuser.me/api')
+      .then(res=> res.json())
+      .then(data => setRandomUser(data.results[0]))
+  },[] )
   const handleLike = () =>{
       const color = likeColor ? '' : 'primary';
       setLikeColor(color);
   }
   return (
-    <div className="App">
+    <div >
       <AccessAlarmIcon></AccessAlarmIcon>
       <ThumbUpIcon onClick={handleLike} color={likeColor}></ThumbUpIcon>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Name:{singleUser.name}</h1>
+      <h2>random name: {randomUser.name?.first} {randomUser.name?.last}</h2>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+
+
+    
     </div>
   );
 }
